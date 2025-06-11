ec46bf7aa1d50d9a423b7454aa86323a1e074729
actbemu <kimura@shokei.ac.jp>
2025-06-11 12:46:56 +0900

    Update auftakt52g.js

--- auftakt52g.js
+++ auftakt52g.js
@@ -684,7 +684,7 @@ myc.addEventListener('mousemove', mcMouseMove);
 	
 	myc.addEventListener('touchend', mcTouchEnd);
 	function mcTouchEnd(event) {
-	dispMsg('Click? isClick:' + isClick);
+	dispMsg('Click? longtap:' + longtap);
 		mousedown = false;
 		if(longtap){
 			touch = false;
